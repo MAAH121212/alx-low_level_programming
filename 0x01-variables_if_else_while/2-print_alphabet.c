6e842b94d4a803daaa1e13d41a5ef74759446f73
Your Name <you@example.com>
2023-07-14 15:29:49 -0700

    pop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all alphabit letters
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+	}
+	putchar("\n");
+
+	return (0);
+}
