11cdf3497bb633310214a7a99cb37daa7bd1a4d5
Your Name <you@example.com>
2023-07-18 19:22:17 -0700

    pop'

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,10 @@
 
 int print_last_digit(int n)
 {
-	int last_digit;
+	int lastdigit;
 
 	if (n < 0)
-		last_digit = -1 * (last_digit);
+		last_digit = -1 * (n % 10);
 	else
 		last_digit =  n % 10;
 
