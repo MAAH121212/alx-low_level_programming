6bd1b0ff761695509c44d4b0b0ef82e68df98a87
Your Name <you@example.com>
2023-07-18 17:58:25 -0700

    pop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabit - utilizes on the _putchar to print
+ * the alphabit a-z
+ */
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+	
+	return (0);
