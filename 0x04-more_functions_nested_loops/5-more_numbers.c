6e1c32508c3a630beaa0c7def7cad55a30c74652
Your Name <you@example.com>
2023-07-20 06:06:22 -0700

    pop

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers
+ */
+
+void more_numbers(void)
+{
+	int i = 1;
+	int c = 0;
+
+	do {
+		if (c <= 14)
+			_putchar(c + 48);
+	} while (i <= 10);
+}
