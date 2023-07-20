59259747c217e3c1cc48a5ae1337e6f664baff19
Your Name <you@example.com>
2023-07-20 06:47:14 -0700

    pop

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
+ */
+
+void print_line(int n)
+{
+	int incar, n;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (incar = 1; incar <= n; incar++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
