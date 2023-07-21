91e15df818a019cfa5ead0c6f2ac71d503627752
Your Name <you@example.com>
2023-07-20 19:40:02 -0700

    op

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int hei, wid;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (hei = 1; hei <= size; hei++)
+		{
+			for (wid = 1; wid <= hei; wid++)
+			{
+				if ((hei + wid) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar(' ');
+		}
+	}
+}
