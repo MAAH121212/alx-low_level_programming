f52d54a8bcdce802a84f5270d3b68896ccf79ffc
Your Name <you@example.com>
2023-07-26 16:16:32 -0700

    pop

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
+ */
+
+char *leet(char *n)
+{
+	char *a = n;
+	char keys[] = {'A', 'E', 'O', 'T', 'L'};
+	int values[] = {4, 3, 0, 7, 1};
+	int i;
+
+	while (*n)
+	{
+		for (i = 0; i < sizeof(keys) / sizeof(values); i++)
+		{
+			if (*n == keys[i] || *n == keys[i] + 32)
+			{
+				*n = values[i] + 48;
+			}
+		}
+		n++;
+	}
+	return (a);
+}
