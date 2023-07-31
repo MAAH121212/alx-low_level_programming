f3628a023cd8e971b3a159bc92226790e5bc540f
Your Name <you@example.com>
2023-07-31 13:07:23 -0700

    pop

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i, p;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (p = 0; p < 8; p++)
+		{
+			_putchar(a[i][p]);
+		}
+		_putchar('\n');
+	}
+}
