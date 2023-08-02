39eb47010d7cd982c79f3d44ab4fca0cf93f01b7
Your Name <you@example.com>
2023-08-02 07:00:52 -0700

    pop

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * _square - a function that expect two integers.
+ * @n: input of type integer.
+ * @val: input of type integer.
+ * Return: int.
+ */
+
+int _square(int n, int val)
+{
+	if (val * val == n)
+	{
+		return (val);
+	}
+	else if (val * val < n)
+	{
+		return (_square(n, val + 1));
+	}
+	else
+	{
+		return (-1);
+	}
+}
+/**
  * _sqrt_recursion - returns the natural square root of a number.
  * @n: an input of type int.
  * Return: The square root of n.
@@ -8,18 +30,5 @@
 
 int _sqrt_recursion(int n)
 {
-	int val = 1;
-
-	if (val * val == n)
-	{
-		return (val);
-	}
-	else if (val * val < n)
-	{
-		return (_sqrt_recursion(n), val + 1);
-	}
-	else
-	{
-		return (-1);
-	}
+	return (_square(n, 1));
 }
