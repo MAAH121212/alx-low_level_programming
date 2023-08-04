1899233388617b07543a388262d63b2f3e181b1a
Your Name <you@example.com>
2023-08-04 06:44:52 -0700

    pop

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print the number of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+		printf("\n");
+	}
+	printf("\n");
+	return (0);
+}
