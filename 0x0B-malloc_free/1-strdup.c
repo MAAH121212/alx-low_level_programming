80ade126324586ed88ca617c4c5e87e7de788e5a
Your Name <you@example.com>
2023-08-08 05:56:01 -0700

    pop

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,13 +14,11 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	for (; str[size] != '\0'; size++)
+	for (; str[size] != '\0'; size++);
 		n = malloc(sizeof(*str) + 1);
 
 	if (n == 0)
-	{
 		return (NULL);
-	}
 	else
 	{
 		for (; i < size; i++)
