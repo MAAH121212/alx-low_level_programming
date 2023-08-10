2b8401254ce825a5d928eda1ea2f4942b800a991
Your Name <you@example.com>
2023-08-10 04:33:36 -0700

    pop

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,9 +19,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (s1_length = 0; s1[s1_length] != '\0'; s1_length)
+	for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
 	;
-	for (s2_length = 0; s2[s2_length] != '\0'; s2_length)
+	for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
 	;
 
 	ptr = malloc(s1_length + n + 1);
