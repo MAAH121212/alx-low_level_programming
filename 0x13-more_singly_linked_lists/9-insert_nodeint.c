310ed8c0ca083f93a10b5ddd6bbe4d3942a4f101
Your Name <you@example.com>
2023-08-29 21:03:16 +0300

    pop

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,11 +12,13 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
+
 	listint_t *temp = *head;
 	listint_t *new;
 
 	new = malloc(sizeof(listint_t));
-	if (!new || !temp)
+
+	if (!new || !head)
 		return (NULL);
 
 	new->n = n;
@@ -28,7 +30,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (new);
 	}
-	
 	for (i = 0; temp && i < idx; i++)
 	{
 		if (i == idx - 1)
