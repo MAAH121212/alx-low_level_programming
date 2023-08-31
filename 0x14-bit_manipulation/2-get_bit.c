122aa63a18c00c402c7dc21a61fc87703225d41b
Your Name <you@example.com>
2023-08-31 18:11:59 +0300

    pop

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+/**
+ * get_bit - returns the value of a bit at an index in a decimal number
+ * @n: number to search
+ * @index: index of the bit
+ *
+ * Return: value of the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bitt =;
+
+	if (index > 63)
+		return (-1);
+
+	bitt = (n >> index) & 1;
+	return (bitt);
+}
