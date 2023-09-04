2ec26ad1f66d3029e6930767a29f2eae2b9afe7a
Your Name <you@example.com>
2023-09-04 19:03:13 +0300

    pop

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * create_file - Creates a file.
@@ -21,8 +22,8 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	fd = open(filename, O_CREATE | O_RDWR | O_TRUNC, 0600);
-	W = write(fd, text_content, len);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
 	
 	if (fd == -1 || w == -1)
 		return (-1);
