a1c908c955479cafaa7997b90bbfb4495b60dec7
Your Name <you@example.com>
2023-09-04 19:15:25 +0300

    pop

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len]; )
+		for (len = 0; text_content[len];)
 			len++;
 	}
 
@@ -28,6 +28,6 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1 || w == -1)
 		return (-1);
 
-	close (fd);
+	close(fd);
 	return (1);
 }
