2c31cf5fed09a5a81e00ae9c98e24e5bd12bb78d
Your Name <you@example.com>
2023-09-04 19:54:55 +0300

    pop

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,6 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 char *create_buffer(char *file);
 void close_file(int fd);
@@ -64,7 +64,7 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	buffer = create_buffer(argv[2]);
-	from = open(argc[1], O_RDONLY);
+	from = open(argv[1], O_RDONLY);
 	r = read(from, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
@@ -73,7 +73,7 @@ int main(int argc, char *argv[])
 		{
 			dprintf(STDERR_FILENO,
 					"Error: Can't read from file %s\n", argv[1]);
-			free(budder);
+			free(buffer);
 			exit(98);
 		}
 		w = write(to, buffer, r);
