9ba01223bc93b840b8fbdec289c9abd5ffbc273d
Your Name <you@example.com>
2023-09-04 19:25:03 +0300

    po{

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
-	p = open(filename, O_WRONLY, O_APPEND);
+	p = open(filename, O_WRONLY | O_APPEND);
 	w = write(p, text_content, len);
 
 	if (p == -1 || w == -1)
